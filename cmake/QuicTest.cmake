3bddebdca3013486dd7c3f755d86b47f73e8abdd
Udip Pant <udippant@fb.com>
2019-05-06 16:50:29 -0700

    always pull in googletest-1.8 as external project
    Summary:
    Relying on the default find for gtest / gmock (via LIBGMOCK_FOUND) is set to
    true if another version of gmock is already installed and can be incompatible
    
    Reviewed By: siyengar
    
    Differential Revision: D15216828
    
    fbshipit-source-id: 94d6e1a14914181842e34bd262b1a46c59155435

--- cmake/QuicTest.cmake
+++ cmake/QuicTest.cmake
@@ -6,48 +6,45 @@
 include(CTest)
 if(BUILD_TESTS)
   include(GoogleTest)
-  find_package(GMock)
-  if(NOT LIBGMOCK_FOUND)
-    include(ExternalProject)
+  include(ExternalProject)
 
-    # Download and install GoogleMock
-    ExternalProject_Add(
-        googletest
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG release-1.8.0
-        PREFIX googletest
-        # Disable install step
-        INSTALL_COMMAND ""
-        LOG_DOWNLOAD ON
-        LOG_UPDATE 1
-        LOG_CONFIGURE ON
-        LOG_BUILD ON
-        LOG_TEST 1
-        LOG_INSTALL 1
-        CMAKE_CACHE_ARGS
-            -DCMAKE_BUILD_TYPE:STRING=Release
-            -DBUILD_GMOCK:BOOL=ON
-            -DBUILD_GTEST:BOOL=ON
-            -Dgtest_force_shared_crt:BOOL=OFF
-    )
- 
-    ExternalProject_Get_Property(googletest source_dir)
-    set(GTEST_SOURCE_DIR ${source_dir})
-    ExternalProject_Get_Property(googletest binary_dir)
-    set(GTEST_BINARY_DIR ${binary_dir})
+  # Download and install GoogleMock
+  ExternalProject_Add(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG release-1.8.0
+    PREFIX googletest
+    # Disable install step
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD ON
+    LOG_UPDATE 1
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+    LOG_TEST 1
+    LOG_INSTALL 1
+    CMAKE_CACHE_ARGS
+      -DCMAKE_BUILD_TYPE:STRING=Release
+      -DBUILD_GMOCK:BOOL=ON
+      -DBUILD_GTEST:BOOL=ON
+      -Dgtest_force_shared_crt:BOOL=OFF
+  )
 
-    # Setup gtest / gmock libraries and include dirs
-    set(LIBGTEST_LIBRARIES
-        "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/gtest/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}"
-        "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/gtest/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}"
-)
-    set(LIBGMOCK_LIBRARIES
-        "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX}"
-        "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/${CMAKE_STATIC_LIBRARY_PREFIX}gmock_main${CMAKE_STATIC_LIBRARY_SUFFIX}"
-)
-    set(LIBGMOCK_INCLUDE_DIR "${source_dir}/googlemock/include")
-    set(LIBGTEST_INCLUDE_DIR "${source_dir}/googletest/include")
- endif()
+  ExternalProject_Get_Property(googletest source_dir)
+  set(GTEST_SOURCE_DIR ${source_dir})
+  ExternalProject_Get_Property(googletest binary_dir)
+  set(GTEST_BINARY_DIR ${binary_dir})
+
+  # Setup gtest / gmock libraries and include dirs
+  set(LIBGTEST_LIBRARIES
+    "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/gtest/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/gtest/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}"
+  )
+  set(LIBGMOCK_LIBRARIES
+    "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/${CMAKE_STATIC_LIBRARY_PREFIX}gmock_main${CMAKE_STATIC_LIBRARY_SUFFIX}"
+  )
+  set(LIBGMOCK_INCLUDE_DIR "${source_dir}/googlemock/include")
+  set(LIBGTEST_INCLUDE_DIR "${source_dir}/googletest/include")
 endif()
 
 function(quic_add_test)
