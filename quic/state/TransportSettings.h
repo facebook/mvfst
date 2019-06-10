f793f66addf0ce92257e6ab9da3671fc017faf38
Luca Niccolini <lniccolini@fb.com>
2019-06-10 07:45:49 -0700

    Back out "[quic] set GSO as default batching mode"
    Differential Revision: D15738809
    
    fbshipit-source-id: f18f9b6dd4686b55786de97f25fceae0afe4554f

--- quic/state/TransportSettings.h
+++ quic/state/TransportSettings.h
@@ -74,7 +74,7 @@ struct TransportSettings {
   // flowControlWindowFrequency * window if the flow control changes.
   uint16_t flowControlWindowFrequency{2};
   // batching mode
-  QuicBatchingMode batchingMode{QuicBatchingMode::BATCHING_MODE_GSO};
+  QuicBatchingMode batchingMode{QuicBatchingMode::BATCHING_MODE_NONE};
   // maximum number of packets we can batch
   // does not apply to BATCHING_MODE_NONE
   uint32_t batchingNum{kDefaultQuicBatchingNum};
