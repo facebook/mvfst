5ae32c37892cfc72f37be7307d95617151281415
Subodh Iyengar <subodh@fb.com>
2019-12-02 17:47:29 -0800

    only execute logger when it is on
    Summary: this gates calling quic logging only until the logger is on. The static trace probes cause the toStrings to be invoked even though the probes are not active. This is not great. We haven't used the probes in a very long time, so let's just disable them
    
    Reviewed By: mjoras
    
    Differential Revision: D18758993
    
    fbshipit-source-id: 4ccce3374cbf431607fc8d5fb5a29410c7f94529

--- quic/logging/QuicLogger.h
+++ quic/logging/QuicLogger.h
@@ -38,10 +38,6 @@ void quicTraceStream(std::string& value, T&& t, Args&&... args) {
 
 template <class T, class... Args>
 void quicTraceLogger(std::string name, const T& conn, Args&&... args) {
-  if (!conn.logger && !VLOG_IS_ON(20)) {
-    return;
-  }
-
   std::string value;
   quicTraceStream(value, std::forward<Args>(args)...);
 
@@ -52,57 +48,21 @@ void quicTraceLogger(std::string name, const T& conn, Args&&... args) {
   }
 }
 
-// We support 11 paramster after name and conn in QUIC_TRACE function, of which
-// 8 will be passed to FOLLY_SDT. Expand the following macros if you need to
-// support more params.
-#define TAKE_1(N1) N1
-#define TAKE_2(N1, N2) N1, N2
-#define TAKE_3(N1, N2, N3) N1, N2, N3
-#define TAKE_4(N1, N2, N3, N4) N1, N2, N3, N4
-#define TAKE_5(N1, N2, N3, N4, N5) N1, N2, N3, N4, N5
-#define TAKE_6(N1, N2, N3, N4, N5, N6) N1, N2, N3, N4, N5, N6
-#define TAKE_7(N1, N2, N3, N4, N5, N6, N7) N1, N2, N3, N4, N5, N6, N7
-#define TAKE_8(N1, N2, N3, N4, N5, N6, N7, N8) N1, N2, N3, N4, N5, N6, N7, N8
-#define TAKE_8_FROM_MORE(N1, N2, N3, N4, N5, N6, N7, N8, ...) \
-  N1, N2, N3, N4, N5, N6, N7, N8
-
-#define PARAM_TAKE_HELPER(                                     \
-    _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, CHOSEN, ...) \
-  CHOSEN
-
-#define TAKE_ATMOST_8(...) \
-  PARAM_TAKE_HELPER(       \
-      __VA_ARGS__,         \
-      TAKE_8_FROM_MORE,    \
-      TAKE_8_FROM_MORE,    \
-      TAKE_8_FROM_MORE,    \
-      TAKE_8,              \
-      TAKE_7,              \
-      TAKE_6,              \
-      TAKE_5,              \
-      TAKE_4,              \
-      TAKE_3,              \
-      TAKE_2,              \
-      TAKE_1)              \
-  (__VA_ARGS__)
-
 #if FOLLY_MOBILE
 #define QUIC_LOGGER(name, conn, ...) (void)conn;
 #else
-#define QUIC_LOGGER(name, conn, ...) quicTraceLogger(#name, conn, __VA_ARGS__);
+#define QUIC_LOGGER(name, connmacro, ...)           \
+  if ((connmacro).logger || VLOG_IS_ON(20)) {       \
+    quicTraceLogger(#name, connmacro, __VA_ARGS__); \
+  }
 #endif
 
 #if QUIC_TPERF
 #define QUIC_TRACE(name, conn, ...) ;
 #else
-#define QUIC_TRACE(name, conn, ...)                                           \
-  do {                                                                        \
-    QUIC_LOGGER(name, conn, __VA_ARGS__)                                      \
-    FOLLY_SDT(                                                                \
-        quic,                                                                 \
-        name,                                                                 \
-        (conn).clientConnectionId.value_or(quic::ConnectionId{{0, 0, 0, 0}}), \
-        TAKE_ATMOST_8(__VA_ARGS__));                                          \
+#define QUIC_TRACE(name, conn, ...)      \
+  do {                                   \
+    QUIC_LOGGER(name, conn, __VA_ARGS__) \
   } while (false);
 #endif
 
