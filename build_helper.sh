ed44020ea3290ba60ca2b4c71eea0633042d6866
Olivier Delalleau <507137+odelalleau@users.noreply.github.com>
2020-02-05 10:59:05 -0500

    Rename FOLDER -> DIR for consistency

--- build_helper.sh
+++ build_helper.sh
@@ -12,7 +12,7 @@
 # Obtain the mvfst repository root folder. Taken from:
 #   https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 # This is done at very start as it won't work if run after switching folders.
-MVFST_ROOT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+MVFST_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Useful constants
 COLOR_RED="\033[0;31m"
@@ -208,7 +208,7 @@ function setup_folly() {
     fi
   fi
 
-  synch_dependency_to_commit $FOLLY_DIR $MVFST_ROOT_FOLDER/build/deps/github_hashes/facebook/folly-rev.txt
+  synch_dependency_to_commit $FOLLY_DIR $MVFST_ROOT_DIR/build/deps/github_hashes/facebook/folly-rev.txt
 
   if [ "$Platform" = "Mac" ]; then
     # Homebrew installs OpenSSL in a non-default location on MacOS >= Mojave
@@ -252,7 +252,7 @@ function setup_fizz() {
     git clone https://github.com/facebookincubator/fizz "$FIZZ_DIR"
   fi
 
-  synch_dependency_to_commit $FIZZ_DIR $MVFST_ROOT_FOLDER/build/deps/github_hashes/facebookincubator/fizz-rev.txt
+  synch_dependency_to_commit $FIZZ_DIR $MVFST_ROOT_DIR/build/deps/github_hashes/facebookincubator/fizz-rev.txt
 
   echo -e "${COLOR_GREEN}Building Fizz ${COLOR_OFF}"
   mkdir -p "$FIZZ_BUILD_DIR"
