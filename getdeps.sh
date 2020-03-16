e1bd93ccd44b88b5a2466d2ddc8e040ab827cf99
Yang Chi <yangchi@fb.com>
2020-03-16 11:07:42 -0700

    Add copyright comment to Quic getdeps script
    Summary: as title
    
    Reviewed By: sharmafb
    
    Differential Revision: D20469531
    
    fbshipit-source-id: ffb84850e4b9d876bc80b1466bd1b1bb28cdee6a

--- getdeps.sh
+++ getdeps.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 set -xeo pipefail
 
 TOOLCHAIN_DIR=/opt/rh/devtoolset-8/root/usr/bin
