8805ec27920f011b2f88bd2af6165a071757f554
Brandon Schlinker <bschlinker@fb.com>
2020-06-22 17:09:11 -0700

    Split TestQuicTransport into separate header
    Summary: Splitting `TestQuicTransport` from `QuicTransportTest` into a separate header file so that it can be reused in other tests, including to support tests that are outside of mvfst.
    
    Reviewed By: lnicco
    
    Differential Revision: D22013646
    
    fbshipit-source-id: 8b6198dca822a95133f517e055a9421b98fe5221

--- quic/api/test/QuicTransportTest.cpp
+++ quic/api/test/QuicTransportTest.cpp
@@ -15,6 +15,7 @@
 #include <quic/api/QuicTransportBase.h>
 #include <quic/api/QuicTransportFunctions.h>
 #include <quic/api/test/Mocks.h>
+#include <quic/api/test/TestQuicTransport.h>
 #include <quic/common/BufUtil.h>
 #include <quic/common/Timers.h>
 #include <quic/common/test/TestUtils.h>
@@ -31,127 +32,6 @@ using namespace testing;
 
 namespace quic {
 namespace test {
-
-class TestQuicTransport
-    : public QuicTransportBase,
-      public std::enable_shared_from_this<TestQuicTransport> {
- public:
-  TestQuicTransport(
-      folly::EventBase* evb,
-      std::unique_ptr<folly::AsyncUDPSocket> socket,
-      ConnectionCallback& cb)
-      : QuicTransportBase(evb, std::move(socket)) {
-    setConnectionCallback(&cb);
-    conn_.reset(new QuicServerConnectionState());
-    conn_->clientConnectionId = ConnectionId({9, 8, 7, 6});
-    conn_->serverConnectionId = ConnectionId({1, 2, 3, 4});
-    conn_->version = QuicVersion::MVFST;
-    aead = test::createNoOpAead();
-    headerCipher = test::createNoOpHeaderCipher();
-  }
-
-  ~TestQuicTransport() override {
-    // we need to call close in the derived class.
-    connCallback_ = nullptr;
-    closeImpl(
-        std::make_pair(
-            QuicErrorCode(LocalErrorCode::SHUTTING_DOWN),
-            std::string("shutdown")),
-        false);
-  }
-
-  QuicVersion getVersion() {
-    auto& conn = getConnectionState();
-    return conn.version.value_or(*conn.originalVersion);
-  }
-
-  void updateWriteLooper(bool thisIteration) {
-    QuicTransportBase::updateWriteLooper(thisIteration);
-  }
-
-  void pacedWrite(bool fromTimer) {
-    pacedWriteDataToSocket(fromTimer);
-  }
-
-  bool isPacingScheduled() {
-    return writeLooper_->isScheduled();
-  }
-
-  void onReadData(
-      const folly::SocketAddress& /*peer*/,
-      NetworkDataSingle&& /*networkData*/) noexcept override {}
-
-  void writeData() override {
-    if (closed) {
-      return;
-    }
-    writeQuicDataToSocket(
-        *socket_,
-        *conn_,
-        *conn_->clientConnectionId,
-        *conn_->serverConnectionId,
-        *aead,
-        *headerCipher,
-        getVersion(),
-        (isConnectionPaced(*conn_)
-             ? conn_->pacer->updateAndGetWriteBatchSize(Clock::now())
-             : conn_->transportSettings.writeConnectionDataPacketsLimit));
-  }
-
-  void closeTransport() override {
-    closed = true;
-  }
-
-  bool hasWriteCipher() const override {
-    return true;
-  }
-
-  std::shared_ptr<QuicTransportBase> sharedGuard() override {
-    return shared_from_this();
-  }
-
-  void unbindConnection() {}
-
-  QuicServerConnectionState& getConnectionState() {
-    return *dynamic_cast<QuicServerConnectionState*>(conn_.get());
-  }
-
-  auto getAckTimeout() {
-    return &ackTimeout_;
-  }
-
-  auto& getPathValidationTimeout() {
-    return pathValidationTimeout_;
-  }
-
-  auto& lossTimeout() {
-    return lossTimeout_;
-  }
-
-  auto& idleTimeout() {
-    return idleTimeout_;
-  }
-
-  CloseState closeState() {
-    return closeState_;
-  }
-
-  folly::HHWheelTimer* getTimer() {
-    return &getEventBase()->timer();
-  }
-
-  void drainImmediately() {
-    drainTimeoutExpired();
-  }
-
-  void setIdleTimerNow() {
-    setIdleTimer();
-  }
-
-  std::unique_ptr<Aead> aead;
-  std::unique_ptr<PacketNumberCipher> headerCipher;
-  bool closed{false};
-};
 
 /**
  * A DeliveryCallback that closes your transport when it's canceled, or when
