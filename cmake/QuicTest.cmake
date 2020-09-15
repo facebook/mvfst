cffb51eade9316101fcbe4c7c91fd06624f3aa5d
Luca Niccolini <lniccolini@fb.com>
2020-09-14 22:02:19 -0700

    enable tests in getdeps
    Summary: properly find the required GMock version (1.8.0) and allow building tests in getdeps
    
    Reviewed By: mjoras
    
    Differential Revision: D16935741
    
    fbshipit-source-id: 46f62511e2feaf553d028e286a862aa5b30393c6

--- cmake/QuicTest.cmake
+++ cmake/QuicTest.cmake
@@ -4,41 +4,10 @@
 # LICENSE file in the root directory of this source tree.
 
 include(CTest)
+
 if(BUILD_TESTS)
-  include(GoogleTest)
-  include(ExternalProject)
-
-  # Download and install GoogleMock
-  ExternalProject_Add(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG release-1.8.0
-    PREFIX googletest
-    # Disable install step
-    INSTALL_COMMAND ""
-    CMAKE_CACHE_ARGS
-      -DCMAKE_BUILD_TYPE:STRING=Release
-      -DBUILD_GMOCK:BOOL=ON
-      -DBUILD_GTEST:BOOL=ON
-      -Dgtest_force_shared_crt:BOOL=OFF
-  )
-
-  ExternalProject_Get_Property(googletest source_dir)
-  set(GTEST_SOURCE_DIR ${source_dir})
-  ExternalProject_Get_Property(googletest binary_dir)
-  set(GTEST_BINARY_DIR ${binary_dir})
-
-  # Setup gtest / gmock libraries and include dirs
-  set(LIBGTEST_LIBRARIES
-    "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/gtest/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}"
-    "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/gtest/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}"
-  )
-  set(LIBGMOCK_LIBRARIES
-    "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX}"
-    "${GTEST_BINARY_DIR}/${CMAKE_CFG_INTDIR}/googlemock/${CMAKE_STATIC_LIBRARY_PREFIX}gmock_main${CMAKE_STATIC_LIBRARY_SUFFIX}"
-  )
-  set(LIBGMOCK_INCLUDE_DIR "${source_dir}/googlemock/include")
-  set(LIBGTEST_INCLUDE_DIR "${source_dir}/googletest/include")
+  find_package(GMock 1.8.1 MODULE REQUIRED)
+  find_package(GTest 1.8.1 MODULE REQUIRED)
 endif()
 
 function(quic_add_test)
@@ -64,39 +33,41 @@ function(quic_add_test)
     # implementation of 'main()' that calls folly::init
     "${QUIC_FBCODE_ROOT}/quic/common/test/TestMain.cpp"
   )
-   # make sure googletest and fizz are built first
-  add_dependencies(${QUIC_TEST_TARGET} googletest)
+
+  set_property(TARGET ${QUIC_TEST_TARGET} PROPERTY ENABLE_EXPORTS true)
+
+  target_include_directories(${QUIC_TEST_TARGET} PUBLIC
+    "${QUIC_TEST_INCLUDES}"
+    ${LIBGMOCK_INCLUDE_DIR}
+    ${LIBGTEST_INCLUDE_DIRS}
+    ${QUIC_EXTRA_INCLUDE_DIRECTORIES}
+  )
+
+  target_compile_definitions(${QUIC_TEST_TARGET} PUBLIC
+    ${LIBGMOCK_DEFINES}
+  )
+
+  target_link_libraries(${QUIC_TEST_TARGET} PUBLIC
+    "${QUIC_TEST_DEPENDS}"
+    ${LIBGMOCK_LIBRARIES}
+    ${GLOG_LIBRARY}
+  )
 
   # Per https://github.com/facebookincubator/mvfst/pull/9, disable some warnings
   # in the tests that come from -Wextra in _QUIC_BASE_COMPILE_OPTIONS.
   target_compile_options(
     ${QUIC_TEST_TARGET} PRIVATE
     ${_QUIC_BASE_COMPILE_OPTIONS}
-    -Wno-sign-compare
-    -Wno-inconsistent-missing-override
-  )
-  target_link_libraries(${QUIC_TEST_TARGET} PRIVATE
-    "${QUIC_TEST_DEPENDS}"
-  )
-  target_include_directories(${QUIC_TEST_TARGET} PRIVATE
-    "${QUIC_TEST_INCLUDES}"
+    "-Wno-sign-compare"
+    "-Wno-inconsistent-missing-override"
   )
 
   gtest_add_tests(TARGET ${QUIC_TEST_TARGET}
     EXTRA_ARGS "${QUIC_TEST_EXTRA_ARGS}"
     WORKING_DIRECTORY ${QUIC_TEST_WORKING_DIRECTORY}
     TEST_PREFIX ${QUIC_TEST_PREFIX}
-  TEST_LIST QUIC_TEST_CASES)
+    TEST_LIST QUIC_TEST_CASES
+  )
 
-  target_link_libraries(${QUIC_TEST_TARGET} PRIVATE
-    ${LIBGMOCK_LIBRARIES}
-    ${LIBGTEST_LIBRARIES}
-  )
-  target_include_directories(${QUIC_TEST_TARGET} PRIVATE
-    ${LIBGMOCK_INCLUDE_DIR}
-    ${LIBGTEST_INCLUDE_DIR}
-    ${QUIC_EXTRA_INCLUDE_DIRECTORIES}
-  )
-  target_compile_definitions(${QUIC_TEST_TARGET} PRIVATE ${LIBGMOCK_DEFINES})
   set_tests_properties(${QUIC_TEST_CASES} PROPERTIES TIMEOUT 120)
 endfunction()
