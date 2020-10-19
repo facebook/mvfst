4b933a8a2799533e7ee5d8ae156899f7a909bb29
Matt Joras <mjoras@fb.com>
2020-10-18 22:32:54 -0700

    Pack holes in quic::AckState
    Summary: As in title.
    
    Reviewed By: lnicco
    
    Differential Revision: D24375167
    
    fbshipit-source-id: 22a218f215a9ce780b3a2bd64f93fa17bcfd366d

--- quic/state/AckStates.h
+++ quic/state/AckStates.h
@@ -18,19 +18,10 @@ namespace quic {
 
 // Ack and PacketNumber states. This is per-packet number space.
 struct AckState {
-  AckBlocks acks;
   // Largest ack that has been written to a packet
   folly::Optional<PacketNum> largestAckScheduled;
-  // Flag indicating that if we need to send ack immediately. This will be set
-  // to true if we got packets with retransmittable data and haven't sent the
-  // ack for the first time.
-  // TODO: upgrade this to be bundled like non-retransmittable packets once we
-  // figured out how to do timeout.
-  bool needsToSendAckImmediately{false};
   // Count of outstanding packets received with only non-retransmittable data.
   uint64_t numNonRxPacketsRecvd{0};
-  // Count of oustanding packets received with retransmittable data.
-  uint8_t numRxPacketsRecvd{0};
   // The receive time of the largest ack packet
   folly::Optional<TimePoint> largestRecvdPacketTime;
   // Latest packet number acked by peer
@@ -41,6 +32,15 @@ struct AckState {
   folly::Optional<PacketNum> largestReceivedAtLastCloseSent;
   // Next PacketNum we will send for packet in this packet number space
   PacketNum nextPacketNum{0};
+  AckBlocks acks;
+  // Flag indicating that if we need to send ack immediately. This will be set
+  // to true if we got packets with retransmittable data and haven't sent the
+  // ack for the first time.
+  // TODO: upgrade this to be bundled like non-retransmittable packets once we
+  // figured out how to do timeout.
+  bool needsToSendAckImmediately{false};
+  // Count of oustanding packets received with retransmittable data.
+  uint8_t numRxPacketsRecvd{0};
 };
 
 struct AckStates {
