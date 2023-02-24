6beae9d280e6bb1e83ab61cce6581250fa9874e4
Konstantin Tsoy <ktsoy@meta.com>
2023-02-24 11:12:37 -0800

    Protect server transport in a callback
    Reviewed By: hanidamlaj
    
    Differential Revision: D43546517
    
    fbshipit-source-id: ede330167c5281ff8975acbf1bdbc27e420597d8

--- quic/server/QuicServerTransport.cpp
+++ quic/server/QuicServerTransport.cpp
@@ -412,6 +412,7 @@ void QuicServerTransport::onCryptoEventAvailable() noexcept {
       VLOG(10) << "Got crypto event after connection closed " << *this;
       return;
     }
+    FOLLY_MAYBE_UNUSED auto self = sharedGuard();
     updateHandshakeState(*serverConn_);
     processPendingData(false);
     // pending data may contain connection close
