46eb6a807f81e4ba48030690e3aac0a2e36c80ae
Joseph Beshay <jbeshay@meta.com>
2023-06-05 20:27:18 -0700

    Update the min_ack_delay codepoint to match draft-ietf-quic-ack-frequency-04
    Summary: The current min_ack_delay codepoint is for draft-00 although we are using the spec from draft-04. This updates the codepoint.
    
    Reviewed By: mjoras
    
    Differential Revision: D46363122
    
    fbshipit-source-id: 2af4b184e52a64d3cfc396c705acd702d062f07a

--- quic/handshake/TransportParameters.h
+++ quic/handshake/TransportParameters.h
@@ -32,7 +32,7 @@ enum class TransportParameterId : uint64_t {
   initial_source_connection_id = 0x000f,
   retry_source_connection_id = 0x0010,
   max_datagram_frame_size = 0x0020,
-  min_ack_delay = 0xff02de1a,
+  min_ack_delay = 0xff04de1a,
   ack_receive_timestamps_enabled = 0xff0a001,
   max_receive_timestamps_per_ack = 0xff0a002,
   receive_timestamps_exponent = 0xff0a003,
