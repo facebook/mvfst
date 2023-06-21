b5f3ae1cc97ad3b147b81bac41e7971e5b1c4fcd
Hani Damlaj <damlaj@meta.com>
2023-06-21 10:07:07 -0700

    decodeQuicInteger
    Summary:
    - refactor `::decodeQuicInteger` to remove unnecessary branching
    - benchmarking shows perf improvement for 8-byte varint
    
    Reviewed By: mjoras
    
    Differential Revision: D46818816
    
    fbshipit-source-id: 292bbfac0b94e60cde64a14e80c11a7fa1d2fe9b

--- quic/codec/QuicInteger.cpp
+++ quic/codec/QuicInteger.cpp
@@ -46,51 +46,40 @@ uint8_t decodeQuicIntegerLength(uint8_t firstByte) {
 folly::Optional<std::pair<uint64_t, size_t>> decodeQuicInteger(
     folly::io::Cursor& cursor,
     uint64_t atMost) {
-  size_t numBytes = 0;
-  size_t advanceLen = 0;
-  uint64_t result = 0;
-
-  if (atMost < 1 || !cursor.canAdvance(1)) {
+  // checks
+  if (atMost == 0 || !cursor.canAdvance(1)) {
     VLOG(10) << "Not enough bytes to decode integer, cursor len="
              << cursor.totalLength();
     return folly::none;
   }
+
+  // get 2 msb of first byte that determines variable-length size expected
   const uint8_t firstByte = *cursor.peekBytes().data();
   const uint8_t varintType = (firstByte >> 6) & 0x03;
-  const uint8_t unmaskedFirstByte = firstByte & 0x3F;
-  uint8_t* resultPtr = reinterpret_cast<uint8_t*>(&result);
+  const uint8_t bytesExpected = (1 << varintType);
 
-  switch (varintType) {
-    case 0:
-      // short circuit for 1 byte.
-      cursor.skip(1);
-      return std::make_pair((uint64_t)unmaskedFirstByte, (size_t)1);
-    case 1:
-      advanceLen = 6;
-      numBytes = 1;
-      break;
-    case 2:
-      advanceLen = 4;
-      numBytes = 3;
-      break;
-    case 3:
-      numBytes = 7;
-      break;
+  // simple short-circuit eval for varint type == 0
+  if (varintType == 0) {
+    cursor.skip(1);
+    return std::pair<uint64_t, size_t>(firstByte & 0x3f, 1);
   }
-  if (atMost < (numBytes + 1) || !cursor.canAdvance(numBytes + 1)) {
-    VLOG(10) << "Could not decode integer numBytes="
-             << static_cast<int>(numBytes + 1) << " firstByte=" << std::hex
-             << static_cast<int>(firstByte);
+
+  // not enough bytes to decode, undo cursor
+  if (!cursor.canAdvance(bytesExpected) || atMost < bytesExpected) {
+    VLOG(10) << "Could not decode integer numBytes=" << bytesExpected;
     return folly::none;
   }
-  cursor.skip(1);
-  memcpy(resultPtr + advanceLen, &unmaskedFirstByte, 1);
-  cursor.pull(resultPtr + advanceLen + 1, numBytes);
-  // make the data dependency on resultPtr explicit to avoid strict
-  // aliasing issues.
-  return std::make_pair(
-      folly::Endian::big(*reinterpret_cast<uint64_t*>(resultPtr)),
-      numBytes + 1);
+  // result storage
+  uint64_t result{0};
+  // pull number of bytes expected
+  cursor.pull(&result, bytesExpected);
+  // clear 2msb bits
+  constexpr uint64_t msbMask = ~(0b11ull << 62);
+  result = folly::Endian::big(result) & msbMask;
+  // adjust quic integer
+  result >>= (8 - bytesExpected) << 3;
+
+  return std::pair<uint64_t, size_t>{result, bytesExpected};
 }
 
 QuicInteger::QuicInteger(uint64_t value) : value_(value) {}
