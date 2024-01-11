0194aa6e55e2cc42f3bb006636f2154b1b934974
Marten Seemann <martenseemann@gmail.com>
2024-01-11 10:59:55 +0700

    echo client: use QUIC v1 (RFC 9000)

--- quic/samples/echo/EchoClient.h
+++ quic/samples/echo/EchoClient.h
@@ -208,6 +208,7 @@ class EchoClient : public quic::QuicSocket::ConnectionSetupCallback,
           qEvb, std::move(sock), std::move(fizzClientContext));
       quicClient_->setHostname("echo.com");
       quicClient_->addNewPeerAddress(addr);
+      quicClient_->setSupportedVersions({QuicVersion::QUIC_V1});
       if (!token.empty()) {
         quicClient_->setNewToken(token);
       }
