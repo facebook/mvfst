70e6ffdd7da18463a4719aa3397a51d269311093
Luca Niccolini <lniccolini@meta.com>
2024-03-25 12:57:16 -0700

    Release MVFST_EXPERIMENTAL version
    Reviewed By: jbeshay
    
    Differential Revision: D55318823
    
    fbshipit-source-id: 08310dc49821f5604f062d42d6f3eb60e09d8a89

--- quic/server/QuicServerWorker.cpp
+++ quic/server/QuicServerWorker.cpp
@@ -650,9 +650,6 @@ QuicServerTransport::Ptr QuicServerWorker::makeTransport(
     }
     trans->setCongestionControllerFactory(ccFactory_);
     trans->setTransportStatsCallback(statsCallback_.get()); // ok if nullptr
-    if (quicVersion == QuicVersion::MVFST_EXPERIMENTAL) {
-      transportSettings_.initCwndInMss = 45;
-    }
 
     auto transportSettings = transportSettingsOverrideFn_
         ? transportSettingsOverrideFn_(
