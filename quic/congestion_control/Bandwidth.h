bd0f1824551970ebbc29ff5dd10a9021854c8629
Joseph Beshay <jbeshay@meta.com>
2025-09-02 10:44:44 -0700

    Remove unimplemented operator overload
    Summary: As title.
    
    Reviewed By: hanidamlaj
    
    Differential Revision: D81359933
    
    fbshipit-source-id: 0b6595020d3baefef92d1efca31b268c0d140095

--- quic/congestion_control/Bandwidth.h
+++ quic/congestion_control/Bandwidth.h
@@ -104,9 +104,6 @@ bool operator>=(const Bandwidth& lhs, const Bandwidth& rhs);
 bool operator==(const Bandwidth& lhs, const Bandwidth& rhs);
 bool operator!=(const Bandwidth& lhs, const Bandwidth& rhs);
 
-template <typename T, typename = std::enable_if_t<std::is_arithmetic<T>::value>>
-Bandwidth operator*(T t, const Bandwidth& bandwidth) noexcept;
-
 uint64_t operator*(std::chrono::microseconds delay, const Bandwidth& bandwidth);
 std::ostream& operator<<(std::ostream& os, const Bandwidth& bandwidth);
 } // namespace quic
